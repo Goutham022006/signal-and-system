ba0d23e1b6d03209fcfaa2ea3322a71f21766500
Goutham022006 <155623996+Goutham022006@users.noreply.github.com>
2024-02-19 15:10:37 +0530

    Add files via upload

--- Signals and systems/11.9.5.6/codes/graph.py
+++ Signals and systems/11.9.5.6/codes/graph.py
@@ -16,6 +16,5 @@ plt.scatter(range(1, len(data) + 1), data, color='orange')
 
 plt.xlabel('Term Number')
 plt.ylabel('Sum of Terms')
-plt.title('Sum of Terms in Arithmetic Progression')
 plt.show()
 
