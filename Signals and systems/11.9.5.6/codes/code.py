66653fec85b4d35da93de6d7550ea200af62d56b
Goutham022006 <155623996+Goutham022006@users.noreply.github.com>
2024-02-22 15:50:22 +0530

    Add files via upload

--- Signals and systems/11.9.5.6/codes/code.py
+++ Signals and systems/11.9.5.6/codes/code.py
@@ -7,7 +7,7 @@ data = np.loadtxt("result_terms.txt", skiprows=1)
 # clear all the previous figures
 plt.close("all")
 
-# extract the first 20 terms of the data
+# extract the first 24 terms of the data
 y_n = data[:24]
 highlighted_index = 21
 # plot the graph
@@ -15,11 +15,9 @@ plt.stem(range(1, len(data) + 1), y_n, markerfmt='bo', linefmt='b-', basefmt='r-
 plt.stem([highlighted_index], [data[highlighted_index - 1]], linefmt='r-', markerfmt='ro', basefmt=' ')
 plt.scatter(range(1, len(data) + 1), data, color='orange',marker='x',s=100,label=r'Analysis')
 # Set labels and title
-plt.xlabel('Term Number')
+plt.xlabel('n')
 plt.ylabel('y(n)')
 plt.xticks(range(1, len(data) + 1))
-plt.title('Simulation v/s Analysis')
-
 # Add legend
 plt.legend()
 plt.grid(True)
