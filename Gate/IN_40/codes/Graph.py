dfbf42568c02915ca6cc18a6a85893c746d75f20
Goutham022006 <155623996+Goutham022006@users.noreply.github.com>
2024-02-29 00:21:45 +0530

    Add files via upload

--- Gate/IN_40/codes/Graph.py
+++ Gate/IN_40/codes/Graph.py
@@ -1,22 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define the function
-def X(s):
-    return 2 * np.exp(-s) / (s**3)
+# import the data from the text file
+data = np.loadtxt("output.txt", skiprows=1)
 
-# Generate values for s
-s_values = np.linspace(0.1, 5, 100)  # Avoiding s=0 for division by zero
+# clear all the previous figures
+plt.close("all")
 
-# Calculate X(s) for each s
-X_values = X(s_values)
-
-# Plot
-plt.figure(figsize=(8, 6))
-plt.plot(s_values, X_values, label=r'$X(s) = \frac{2e^{-s}}{s^3}$')
-plt.xlabel('s')
-plt.ylabel('X(s)')
-plt.title('Plot of X(s)')
+# extract the first 24 terms of the data
+y_n = data[:10]
+highlighted_index = 2
+# plot the graph
+plt.stem(range(1, len(data) + 1), y_n, markerfmt='bo', linefmt='b-', basefmt='r-',label=r'Simulation') 
+plt.stem([highlighted_index], [data[highlighted_index - 1]], linefmt='r-', markerfmt='ro', basefmt=' ')
+plt.scatter(range(1, len(data) + 1), data, color='orange',marker='x',s=100,label=r'Analysis')
+# Set labels and title
+plt.xlabel('n')
+plt.ylabel('y(n)')
+plt.xticks(range(1, len(data) + 1))
+# Add legend
 plt.legend()
 plt.grid(True)
 plt.savefig("fig1.png")
